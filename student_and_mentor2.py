8c35a0a073ecb52f742e7d2b466b19e7fad5a8cf
Vladimir <sas00877@gmail.com>
2024-06-11 23:57:55 +0200

    student_and_mentor2.py

--- student_and_mentor2.py
+++ student_and_mentor2.py
@@ -6,6 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return "Ошибка"
 
 class Reviewer:
     def __init__(self, name, surname):
@@ -21,9 +29,6 @@ class Reviewer:
         else:
             return "Ошибка"
 
-class Lecturer:
-    def __init__(self, student, course, grade):
-        super().__init__(name, surname)
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -36,3 +41,24 @@ cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+class Lecturer:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+best_lecturer = Lecturer('Roy', 'Ema', 'your_gender')
+best_lecturer.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_lecturer, 'Python', 10)
+cool_reviewer.rate_hw(best_lecturer, 'Python', 10)
+cool_reviewer.rate_hw(best_lecturer, 'Python', 10)
+
+print(best_lecturer.grades)
